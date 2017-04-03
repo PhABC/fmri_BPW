30f4342e574d0637b239d4e800b22dbdd13f0568
PhABC <ph.castonguay@gmail.com>
2017-04-03 02:53:31 -0400

    Initial functions and models

---
+++ functions.py
@@ -0,0 +1,218 @@
+#Functions
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+def corrSpacePlot(pos, colVal, plot3D = False, title = '', target = False):
+    '''
+    Will plot in 2d the voxels with color related to a color value 'colVal'.
+    
+    pos   : Matrix (nvox x 3) containg the XYZ cordinate of each voxel.
+            The columns have to be representing X Y Z. 
+    
+    colVal: Value between -1 and 1 that will determine the color of each voxel 
+            (from blue to red).
+    
+    '''
+    
+    #Initializing figure
+    fig = plt.figure( figsize = (15,10))
+    
+    if not plot3D:
+        plt.subplot(2,2,1)
+        xx = plt.scatter(pos[:,0],pos[:,1], c = colVal, cmap = 'jet')
+        plt.xlabel('X', fontsize = 20)
+        plt.ylabel('Y', fontsize = 20)
+        plt.colorbar(xx)
+
+        if target:
+            plt.scatter(pos[target,0],pos[target,1], 
+            c = 'deeppink', marker = '*', s = 500)
+
+        plt.subplot(2,2,2)
+        yy = plt.scatter(pos[:,0],pos[:,2], c = colVal, cmap = 'jet')
+        plt.xlabel('X', fontsize = 20)
+        plt.ylabel('Z', fontsize = 20)
+        plt.colorbar(yy)
+
+        if target:
+            plt.scatter(pos[target,0], pos[target,2], 
+                c = 'deeppink', marker = '*', s = 500)
+
+        plt.subplot(2,2,3)
+        zz = plt.scatter(pos[:,1],pos[:,2], c = colVal, cmap = 'jet')
+        plt.xlabel('Y', fontsize = 20)
+        plt.ylabel('Z', fontsize = 20)
+        plt.colorbar(zz)
+
+        if target:
+            plt.scatter(pos[target,1], pos[target,2], 
+                c = 'deeppink', marker = '*', s = 500)
+
+    else:
+        ax  = fig.gca(projection="3d")
+
+        ax.scatter(pos[:,0],pos[:,1],pos[:,2], c = colVal, s = 12, cmap = 'jet')
+        ax.set_xlabel('X', fontsize = 20)
+        ax.set_ylabel('Y', fontsize = 20)
+        ax.set_zlabel('Z', fontsize = 20)
+
+        #Putting target as pink X
+        if target:
+            ax.scatter(pos[target,0],pos[target,1],pos[target,2], 
+                c = 'deeppink', marker = '*', s = 500)
+
+    
+    plt.suptitle(title, fontsize = 30)
+
+
+
+
+def dictUnpack(model, P):
+    'Will unpack parameter dictionnary to pass in model in correct order'
+    
+    #Model arguments
+    args = model.order
+    keys = list(P.keys())
+    
+    #Will hold correct index for keys wrt to args
+    idx = [0]*len(args)
+    
+    #Finding indexes to order keys
+    for i in range(len(args)):
+        if args[i] in keys:
+            idx[i] = keys.index(args[i])
+
+    argList =   [P[keys[i]]for i in idx]
+    
+    return argList
+
+
+def euclidDmat(X):
+    'Calculate the euclidean distance between rows of matrix p x n'
+    
+
+    if len(X.shape) == 1:
+        p = len(X)
+        X = X.copy().reshape(p,1)
+
+    else:
+        #Shape
+        p, n = X.shape 
+    
+    #Sum of squared tiled
+    SSx = np.tile(np.diag(np.dot( X, X.T )), (p,1)) #sum square X
+    
+    #Distance
+    D = np.sqrt(SSx.T - 2*np.dot( X, X.T ) + SSx)
+    
+    return D
+
+
+
+def filterIdx(dat, idx):
+    '''
+    Will remove index of all the matrices in the dictionnary dat, by keeping idx.
+    
+    dat : Dictionnary with relevant datasets
+    idx : Indexes of voxels to keep
+
+    '''
+    
+    #Going over every data
+    
+    #Taking only relevant indexes
+    D = {d : dat[d][idx,...] for d in dat}
+    
+    return D
+
+
+def matrixSorting(X):
+    'Will sort matrix based on projection on diagonal'
+    
+    #Projection on diagonal
+    xDiag = np.sum(X**2, axis = 1)
+    
+    #Sorting indexes
+    idx   = np.argsort(xDiag)
+    
+    #Sorting
+    Xsort = X[idx,:]
+    
+    return Xsort, idx
+
+
+def nearPSD(A, epsilon = 0):
+    'Nearest positive semi-definite Matrix'
+    
+    n = A.shape[0]
+    eigval, eigvec = np.linalg.eig(A)
+    
+    val = np.matrix(np.maximum(eigval,epsilon))
+    vec = np.matrix(eigvec)
+    T   = 1/(np.multiply(vec,vec) * val.T)
+    T   = np.matrix(np.sqrt(np.diag(np.array(T).reshape((n)) )))
+    B   = T * vec * np.diag(np.array(np.sqrt(val)).reshape((n)))
+    out = B*B.T
+    
+    return(out)
+
+
+def plotCovs(C,Chat):
+
+    plt.figure(figsize = (10,5))
+
+    plt.subplot(1,2,1)
+    plt.imshow(C)
+    plt.title('True Covariance', fontsize = 20)
+    plt.colorbar()
+
+    plt.subplot(1,2,2)
+    plt.imshow(Chat)
+    plt.title('Estimated Covariance', fontsize = 20)
+    plt.colorbar()
+    plt.show()
+    
+    
+def sampleCov(E, df):
+    'Will calculate the sample covariance of residuals (mean 0)'
+    
+    return np.dot(E.T, E)/df
+
+
+def scaleMod(Y, a, b):
+    '''
+    Fitting Yhat positive and negative overshoot
+    
+    Y : Predicted Y
+    a : scalar for positive scaling
+    b : scalar for negative scaling
+    
+    '''
+    Yscale = Y.copy()
+    
+    #Scaling positive and negative
+    Yscale[Y > 0] = Y[Y > 0] * a
+    Yscale[Y < 0] = Y[Y < 0] * b
+    
+    return Yscale
+    
+    
+def simDist(p):
+    'Simulate a distance matrix'
+    
+    #XYZ position of voxels
+    XYZ = np.random.randn(p, 3) + 7; 
+    
+    #Sorting based on projection on diagonal
+    XYZ, _ = matrixSorting(XYZ)
+    
+    #Euclidean distance between voxels
+    D = euclidDmat(XYZ)
+    
+    return D
+    
+
---
+++ models.py
@@ -0,0 +1,163 @@
+import numpy as np
+import scipy as sp
+
+from PWoptim.functions import *
+
+
+class model(object):
+    
+    def __init__(self, name):
+        'Add the order of parameter'
+        
+        #Order of input argument for each model
+        if name == 'euclidC':
+            self.order = ['sd1', 'lamb1']
+                          
+            self.yhat = self._euclidC
+        else:
+            raise Exception('Name of model does not refer to any known model')
+    
+    '''
+                        DEFINING MODELS
+    
+    Each model must contain as a first argument the input (e.g. the distance matrix)
+    and the other argument should be *P, a list of argument. The ordered list of argument
+    should be added in the __init__ function for each model.
+            
+    '''
+    
+    def _euclidC(self, D, P):
+    
+        '''
+        Euclidean distance covariance estimation
+        
+        D : Distance matrix between channels.
+        P : Dictionnary of variables
+        
+                'sd'   : Standard deviation controlling strength.
+                'lamb' : Correlation floor.
+
+        '''
+        
+        #If each voxel is standardize, then correlation == cov
+        #since cov_ij = corr_ij * sqrt(sd_i * sd_j)
+        
+        #Unpacking dictionnary (changing locals() doesn't work inside functions ...)
+        sd = P['sd']; lamb = P['lamb']
+
+        
+        #Correlation modelling
+        C = np.minimum(np.exp(-D / (2 * sd)) + lamb, 1)
+
+        return C
+    
+    
+    
+def costFunction(P, modelName, X, Y):
+
+    '''
+    Cost function based on the least-squared errors. Need to calculate the residuals.
+
+    P : Dictionnary containing parameters. Name of fields have to match name of 
+        variables in the model.
+    X : Input data
+    Y : Actual data
+
+    modelName : Name of model to use, which will take P parameter as input.
+    '''
+
+    #Building model
+    m = model('euclidC')
+
+    #Calculating predicted Y
+    Yhat = m.yhat(X, P)
+
+    return (Y - Yhat) + np.sum(Yhat[abs(Yhat) > 1])*100
+
+
+
+
+def crossValFit(X, D, P, model):
+    ''' 
+    Will cross-validate a model (or a list of model)
+    
+    X     : Data with shape (nvox,npoints,nruns)
+    D     : Distance (or other feature) matrix
+    P     : Parameter dictionnary for minimize function
+    model : Model name
+    '''
+    
+    #Dimensions
+    nvox, ntime, nruns = X.shape
+    
+    #Initializing
+    Res  = np.zeros((nruns, nvox**2))
+    MSE  = np.zeros(nruns)
+    Phat = [None]*nruns 
+    
+    #Loop over all run with 1 being test set
+    for r in np.arange(nruns):
+        
+        #Mask for indexing (excluding test)
+        M = np.ones(nruns); M[r] = 0; M = M == 1
+        
+        #Test sample-covariance
+        Ctest = np.corrcoef(X[:,:,r])
+        
+        #Arguments for minimize function
+        args = (model,             # Model name
+                np.hstack(D),      # Distance (or input to model) 
+                np.hstack(Ctest))  # Sample correlation/covariance matrix
+        
+        #Fitting model on current run
+        out = minimize(costFunction, P, args = args)
+        
+        #Extracting estimated parameters
+        Phat[r] = {k : out.params[k].value for k in out.params.keys()}
+        
+        #Covariance modelling to test fit
+        S = simNoise(1, D, Phat[r], modelName = model)
+        
+        #Residuals
+        Res[r,:] = np.hstack(Ctest - S['C'])
+        
+        #Mean-Square error
+        MSE[r] = np.mean( (Res[r,:])**2)
+        
+        corr   = np.corrcoef(np.hstack(Ctest), np.hstack(S['C']))
+        
+        print('Fold {} of {} : corr ~ {:.2f}'.format(r+1, nruns, corr[0,1]))
+        
+    return MSE, Res, Phat
+
+
+
+def simNoise(n, D, modelParam, modelName = 'euclidC'):
+    '''Noise simulation
+    
+    p : Number of dimensions (voxels)
+    n : Number of samples
+    
+    modelParam : List of parameters for the model (right order), 
+                 containing name of model.
+    
+    '''
+    
+    #Covariance matrix
+    #print('Creating covariance matrix          ... \n\n')
+    
+    #Number of voxels
+    p =D.shape[0]
+
+    #Creating model
+    m = model(modelName)
+    
+    #Covariance
+    C = m.yhat(D, modelParam)
+
+    #Simulating noise
+    E = np.random.multivariate_normal([0]*p, C, n)
+    
+    return {'E': E, 'C': C}
+
+
